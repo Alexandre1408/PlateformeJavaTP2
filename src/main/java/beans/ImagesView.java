3a461cea907f45bdba2ed73ce43c60fe2b41a4f2
acutemihawk <57567300+acutemihawk@users.noreply.github.com>
2021-06-05 15:57:42 +0200

    Update ImagesView.java

--- src/main/java/beans/ImagesView.java
+++ src/main/java/beans/ImagesView.java
@@ -8,14 +8,27 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
-
+/**
+ * Classe utilisée pour afficher les images du contentFlow (les joueurs formés par nous-même)
+ * @author BELDA Tom, INGARAO Adrien, MAGGOUH Naoufal, UNG Alexandre
+ */
 @Named
 @RequestScoped
 public class ImagesView implements Serializable
 {
+	/**
+	 * Utilisé car la classe implement serializable
+	 */
 	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Liste des images qui seront affichées dans le contentFlow
+	 */
 	private List<String> images;
-
+	
+	/**
+	 * Initialise les données de la liste
+	 */
     @PostConstruct
     public void init()
     {
@@ -26,7 +39,11 @@ public class ImagesView implements Serializable
         images.add("Bikinibodhi");
         images.add("ZywOo");
     }
-
+    
+    /**
+     * Getter de la liste des noms des images à afficher
+     * @return la liste des noms des images à afficher
+     */
     public List<String> getImages()
     {
         return images;
